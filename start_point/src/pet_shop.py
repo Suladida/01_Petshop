31de1e0fd0e0c2269cb7fc98f2f078f1dc340953
Robyn Tyler <robynstyler@outlook.com>
2020-11-06 21:33:23 +0000

    Added Cash

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -3,8 +3,9 @@
 def get_pet_shop_name(petshop_name):
     return petshop_name["name"]
 
-
 def get_total_cash(total_cash):
     return(total_cash["admin"]["total_cash"])
 
+def add_or_remove_cash(pet_shop_list, new_cash):
+    pet_shop_list["admin"] ["total_cash"] += new_cash
 
